cd5adacceb6427e2f21bd127e13d399854b3ff8b
Philip Masser <philip.masser@goodson.at>
2019-10-21 17:58:05 +0200

    Added Flexform for changing instance specific template file

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,6 +7,10 @@
     'EXT:ms3commercefx/Resources/Public/Icons/Extension.png'
 );
 
-
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ms3commercefx_pi1']='pages,recursive';//layout,select_key,
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ms3commercefx_pi1']='pages,recursive';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['ms3commercefx_pi1']='pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'ms3commercefx_pi1',
+    'FILE:EXT:ms3commercefx/Configuration/FlexForms/Ms3CommerceFxPi1.xml'
+);
 
