c8dc351b68153a11032fe1fe48196e20d573b8a4
Philip Masser <philip.masser@goodson.at>
2019-10-21 16:01:44 +0200

    Add commerce to content element wizard

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -6,3 +6,7 @@
     'mS3 Commerce Fx',
     'EXT:ms3commercefx/Resources/Public/Icons/Extension.png'
 );
+
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ms3commercefx_pi1']='pages,recursive';//layout,select_key,
+
