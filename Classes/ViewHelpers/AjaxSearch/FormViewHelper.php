0dc34954a11f57c2684c8e892727fe1872512a97
Alejandro Olvera <alejandro.olvera@goodson.at>
2021-11-18 10:18:56 +0000

    Fix for Typo3 10: TagBuilder alias remove in Typo3 Fluid Core

--- Classes/ViewHelpers/AjaxSearch/FormViewHelper.php
+++ Classes/ViewHelpers/AjaxSearch/FormViewHelper.php
@@ -21,7 +21,7 @@ use Ms3\Ms3CommerceFx\ViewHelpers\AbstractTagBasedViewHelper;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
-use TYPO3\CMS\Fluid\Core\ViewHelper\TagBuilder;
+use TYPO3Fluid\Fluid\Core\ViewHelper\TagBuilder;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 
 class FormViewHelper extends AbstractTagBasedViewHelper
