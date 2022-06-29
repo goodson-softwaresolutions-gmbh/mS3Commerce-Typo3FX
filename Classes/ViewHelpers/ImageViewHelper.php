9b651e1212fc7e2207943a838f8ccdbab36e670a
Philip Masser <philip.masser@goodson.at>
2022-06-29 12:38:11 +0200

    Enable placeholder image for image view helper

--- Classes/ViewHelpers/ImageViewHelper.php
+++ Classes/ViewHelpers/ImageViewHelper.php
@@ -9,11 +9,42 @@ class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
     {
         parent::initializeArguments();
         $this->registerArgument('srcOnly', 'bool', 'if true, only returns the URL, otherwise image tag', false, false);
+        $this->registerArgument('placeholder', 'string', 'Replacement image if image doesn\'t exist', false);
+        $this->registerArgument('placeholderTransform', 'bool', 'if true (default), parameters will also be applied to placeholder', false, true);
     }
 
     public function render()
     {
+        $args = $this->arguments;
+        $placeholder = $args['placeholder'];
+        $trans = $args['placeholderTransform'];
+        unset($args['placeholder']);
+        unset($args['placeholderTransform']);
+        $res = $this->doRender($args);
+
+        if (!$res && $placeholder) {
+            $args['src'] = $placeholder;
+            unset($args['file']);
+            if (!$trans) {
+                unset($args['crop']);
+                unset($args['cropVariant']);
+                unset($args['fileExtension']);
+                unset($args['width']);
+                unset($args['height']);
+                unset($args['minWidth']);
+                unset($args['minHeight']);
+                unset($args['maxWidth']);
+                unset($args['maxHeight']);
+            }
+            $res = $this->doRender($args);
+        }
+        return $res;
+    }
+
+    private function doRender($args)
+    {
         try {
+            $this->arguments = $args;
             $res = parent::render();
             if ($this->arguments['srcOnly']) {
                 return $this->tag->getAttribute('src');
@@ -21,7 +52,7 @@ class ImageViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\ImageViewHelper
                 return $res;
             }
         } catch (\Exception $e) {
-            return "";
+            return '';
         }
     }
 }
